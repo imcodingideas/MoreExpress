25b54deb78618dce09b74e48ef7a6eeb71574c17
Joseph Chambers <joseph@michael-chambers.com>
2016-11-03 22:50:50 -0700

    javascript in ejs, and passing objects through the template

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/fallinlovewith/:thing', function (req, res) {
    res.render('love.ejs', {thing: thing});
 });
 
+app.get('/posts', function (req, res) {
+   var posts = [
+      {title: "Post 1", author: "Joseph"},
+      {title: "Post 2", author: "Angelica"},
+      {title: "Post 3", author: "Brandon"}
+   ];
+
+   res.render('posts.ejs', {posts: posts})
+});
+
 app.listen(3000, function () {
    console.log('The server is running');
 });
