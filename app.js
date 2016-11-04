123e46e50a33c822517ebab769c6cc29612164c0
Joseph Chambers <joseph@michael-chambers.com>
2016-11-03 22:27:39 -0700

    added ejs, and added dynamic templates

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello');
+   res.render('home.ejs');
+});
+
+app.get('/fallinlovewith/:thing', function (req, res) {
+   var thing = req.params.thing;
+   res.render('love.ejs', {thing: thing});
 });
 
 app.listen(3000, function () {
